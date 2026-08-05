4de7add62267e42901dd0254212fd144c1e768fb
alien boy <166526757+alienmoom@users.noreply.github.com>
2026-08-05 22:24:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=658dea88-5fb7-458c-8981-085672a8fcc5",
     # 添加更多链接
 ]
 
